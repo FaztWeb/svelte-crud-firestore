b19e57cab8086c680fe355b04d3c3c81f1f39262
fazt <fazt@faztweb.com>
2022-02-15 00:20:49 -0500

    ignored some firebase files

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyBqONMb27lsY2Jlnz8v_Q7LvFZ4TU2jDu8",
-  authDomain: "fir-app-ea974.firebaseapp.com",
-  projectId: "fir-app-ea974",
-  storageBucket: "fir-app-ea974.appspot.com",
-  messagingSenderId: "798169977201",
-  appId: "1:798169977201:web:c12541e727e6bd7119a6d0",
+  // put your credentials here
 };
 
 // Initialize Firebase
